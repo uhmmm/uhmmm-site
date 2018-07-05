df61e06e92b6893a19db791cec4162abbfcc4606
Zaturrby <rjkorteschiel@gmail.com>
2018-07-05 13:36:14 +0200

    Gatsbu v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
+import { graphql } from 'gatsby'
 
-const IndexPage = () => (
+import Header from '../components/header'
+import './index.module.css'
+
+const IndexPage = ({ data }) => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header siteTitle={data.site.siteMetadata.title} />
+
+    <main
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </main>
   </div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
